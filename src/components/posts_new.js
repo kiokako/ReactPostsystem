31c4d333626ff2fb370743713dfc1368897a7414
cry004 <cry004@gmail.com>
2017-06-09 21:24:31 +0900

    First push

---
+++ src/components/posts_new.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+
+class PostsNew extends Component {
+  render(){
+    return (
+      <div>
+        Posts New
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
